407aba6809f9d4a059472457d2c0098614c4410b
ryota-sato07 <satour.0722@gmail.com>
2023-05-19 09:20:11 +0900

    設定ファイル、変数、出力値の追加

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
+variable "example_instance_type" {
+  default = "t3.micro"
+}
+
 resource "aws_instance" "sample" {
   ami           = "ami-0c3fd0f5d33134a76"
-  instance_type = "t3.micro"
+  instance_type = var.example_instance_type
+}
 
-  user_data = <<EOF
-    #!/bin/bash
-    yum install -y httpd
-    systemctl start httpd.service
-EOF
+output "example_instance_id" {
+  value = aws_instance.sample.id
 }
