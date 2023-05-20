4b0db8780b05a852da3abb4fbef812416777049d
ryota-sato07 <satour.0722@gmail.com>
2023-05-20 10:37:48 +0900

    組み込み関数の追加

--- 20-sample/01-ec2/main.tf
+++ 20-sample/01-ec2/main.tf
@@ -5,6 +5,7 @@ variable "example_instance_type" {
 resource "aws_instance" "sample" {
   ami           = "ami-0c3fd0f5d33134a76"
   instance_type = var.example_instance_type
+  user_data     = file("./user_data.sh")
 }
 
 output "example_instance_id" {
