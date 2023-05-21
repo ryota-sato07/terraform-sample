dbe8eda1d7cf9b40f33305b32ef4ed4c7c44cfa1
ryota-sato07 <satour.0722@gmail.com>
2023-05-21 09:59:23 +0900

    module作成

--- 20-sample/01-ec2/main.tf
+++ 20-sample/01-ec2/main.tf
@@ -1,13 +1,8 @@
-variable "example_instance_type" {
-  default = "t3.micro"
+module "web_server" {
+  source        = "../../modules"
+  instance_type = "t3.micro"
 }
 
-resource "aws_instance" "sample" {
-  ami           = "ami-0c3fd0f5d33134a76"
-  instance_type = var.example_instance_type
-  user_data     = file("./user_data.sh")
-}
-
-output "example_instance_id" {
-  value = aws_instance.sample.id
+output "public_dns" {
+  value = module.web_server.public_dns
 }
